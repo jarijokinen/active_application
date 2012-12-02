4720ce067ac4a42a28076088b380de9e876de26b
Jari Jokinen <info@jarijokinen.com>
2012-12-02 18:20:17 +0200

    Add route addition step to install generator

--- lib/generators/active_application/install/install_generator.rb
+++ lib/generators/active_application/install/install_generator.rb
@@ -4,6 +4,7 @@ module ActiveApplication
       class_option :user_model,             type: :string, default: "User"
       class_option :authentication_engine,  type: :string, default: "devise"
       class_option :authorization_engine,   type: :string, default: "cancan"
+      class_option :skip_routes,            type: :boolean, default: false
 
       def install_authentication
         if options[:authentication_engine] == "devise"
@@ -14,6 +15,12 @@ module ActiveApplication
       def install_authorization
         if options[:authorization_engine] == "cancan"
           generate "active_application:cancan #{options[:user_model].classify}"
+        end
+      end
+
+      def add_routes
+        unless options[:skip_routes] 
+          inject_into_class "config/routes.rb", "  active_application_routes"
         end
       end
     end
