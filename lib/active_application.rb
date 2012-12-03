8af312d3c4aeaf130b93f291b6a87001b5862b4c
Jari Jokinen <info@jarijokinen.com>
2012-12-03 13:55:07 +0200

    Remove factory_girl_rails require

--- lib/active_application.rb
+++ lib/active_application.rb
@@ -12,10 +12,6 @@ require "active_application/engine"
 require "active_application/routes"
 require "active_application/version"
 
-unless Rails.env.production?
-  require "factory_girl_rails"
-end
-
 module ActiveApplication
   ActionDispatch::Routing::Mapper.send(:include, ActiveApplication::Routes)
 end
